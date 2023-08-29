5acbce9161e5078b2dac65dd2253f23f20218f45
Robert Waffen <rw@betadots.de>
2023-08-29 15:34:09 +0200

    add external py to get the tags

---
+++ get_the_tags.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+# ARGs
+github_repository = sys.argv[1]
+github_ref_name   = sys.argv[2]
+
+image   = f'ghcr.io/{github_repository}'
+tags    = set()
+version = github_ref_name
+
+if version.startswith('v'):
+  version = github_ref_name.replace("v", "")
+  tags.add(f"{image}:latest")
+
+if version == 'main':
+  version = "development"
+
+tags.add(f"{image}:{version}")
+tags = ",".join(sorted(list(tags)))
+
+with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+  print(f'tags={tags}', file=fh)
