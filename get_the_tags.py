747ca36fdaa580f472c8c6ab7b71454c96754d19
Robert Waffen <rw@betadots.de>
2023-08-29 15:52:53 +0200

    use argparse and debug logging

--- get_the_tags.py
+++ get_the_tags.py
@@ -2,24 +2,41 @@
 
 import os
 import sys
+import argparse
+import logging
 
-# ARGs
-github_repository = sys.argv[1]
-github_ref_name   = sys.argv[2]
 
-image   = f'ghcr.io/{github_repository}'
+parser = argparse.ArgumentParser(description='Get the tags!')
+parser.add_argument("-l", "--log", dest="logLevel", choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help="Set the logging level")
+parser.add_argument("--ref", dest="github_ref_name")
+parser.add_argument("--repo", dest="github_repository")
+
+args = parser.parse_args()
+
+if args.logLevel:
+    logging.basicConfig(level=getattr(logging, args.logLevel))
+
+logging.debug(f'ARGS: {args}')
+
+image   = f'ghcr.io/{args.github_repository}'
+logging.debug(f'Image : {image}')
+
 tags    = set()
-version = github_ref_name
+version = args.github_ref_name
 
 if version.startswith('v'):
-  version = github_ref_name.replace("v", "")
+  version = args.github_ref_name.replace("v", "")
   tags.add(f"{image}:latest")
 
 if version == 'main':
   version = "development"
 
+logging.debug(f'Version: {version}')
+
 tags.add(f"{image}:{version}")
 tags = ",".join(sorted(list(tags)))
+
+logging.debug(f'Tags: {tags}')
 
 with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
   print(f'tags={tags}', file=fh)
